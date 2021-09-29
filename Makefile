8d186477b7e5e669ab956f8bd395533cf288e733
joshstenis <joshstenis@gmail.com>
2021-09-29 20:14:38 +0000

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: project0
+
+project0: 
+	gcc project0.c -o project0
+
+clean:
+	rm project0
---
+++ project0.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) {
+	printf("Count characters.\n");
+	return  0;
+}
