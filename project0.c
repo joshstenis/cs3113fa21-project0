d3a6a4563e6f057b3ae8646e995a4745c4820484
joshstenis <joshstenis@gmail.com>
2021-10-04 22:25:07 +0000

    Removed <sys/stat.h> inclusion; edited README with new documentation usages and new bugs/fixes.

--- project0.c
+++ project0.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+//#include <sys/stat.h>
 
 #define MAX_BUF 1024
+
+struct codepoint {
+	int count;
+	char c;
+};
+typedef struct codepoint codepoint;
 
 int main(int argc, char **argv) {
 	
@@ -12,22 +18,22 @@ int main(int argc, char **argv) {
 	ssize_t numRead;
 	char buf[MAX_BUF];
 
+	flagsout = O_CREAT | O_WRONLY | O_TRUNC;
+	perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;	// rw-rw-rw-
+	
 	filein = open(argv[1], O_RDONLY, perms);
 	if (filein == -1) { printf("Error opening input file.\n"); }		// open() error check
 
-	flagsout = O_CREAT | O_WRONLY | O_TRUNC;
-	perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;	// rw-rw-rw-
-	fileout = open(argv[2], flagsout, perms);
-	if (filein == -1) { printf("Error opening output file.\n"); }		// open() error check
 
 	while ((numRead = read(filein, buf, MAX_BUF)) > 0) {			// Count characters in filein
-		if(write(fileout, buf, numRead) != numRead) { printf("Error writing to output file.\n"); }		
+		// Count characters
+		// IDEA: Store to array of structs containing character and its count?	
+		printf("Buffer: %s\n", buf);
 	}
 	
 	if(numRead == -1) { printf("Error reading input file.\n"); }		// read() error check
-	if(close(filein) == -1) { printf("Error closing input file.\n"); }	// close() error checks
-	if(close(fileout) == -1) { printf("Error closing output file.\n"); }
-	
+	if(close(filein) == -1) { printf("Error closing input file.\n"); }	// close() error check
+
 
 
 	// ---- QSORT ----
