33e72320871dedddb2f0980d67e0e605bc50dc89
joshstenis <joshstenis@gmail.com>
2021-10-07 00:02:47 +0000

    Tried to bring back concept of a struct (Char) to keep track of each characters count. Also added a wrapper struct for a Char[], with methods such as contains(), find(), and add(). Needless to say these structs are not yet functional.

--- project0.c
+++ project0.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define MAX_BUF 1024
+
+struct Char {
+	int count, c;
+};
+typedef struct Char Char;
+
+struct String {
+	int len = 0;
+	int size;
+	Char str[size];
+
+	int contains(Char c) {
+		for (int i=0; i < size; i++) {
+			if (str[i].c == (int)c.c) { return 1; }
+		} return 0;
+	}
+
+	int contains(char c) {
+		for (int i=0; i < size; i++) {
+			if (str[i].c == (int)c) { return 1; }
+		} return 0;
+	}
+
+	Char find(char c) {
+		for (int i=0; i < size; i++) {
+			if (str[i].c == (int)c) { return str[i]; }
+		} return NULL;
+	}
+
+	int add(char c, int count) {
+		Char tmp;
+		tmp.c = c;
+		tmp.count = count;
+		if (len > 0 && len < size) {
+			str[len] = tmp;
+			len++;
+			return 0;
+		} else { return -1; }
+	}
+
+	void toString() {
+		for (int i=0; i < size; i++) { printf("Characters: %c\n", str[i].c); }
+	}
+};
+typedef struct String String;
 
 
 int main(int argc, char **argv) {
@@ -11,7 +57,8 @@ int main(int argc, char **argv) {
 	mode_t perms;
 	ssize_t numRead;
 	char buf[MAX_BUF+1];
-
+	buf[MAX_BUF] = "\0";
+	
 	flagsout = O_CREAT | O_WRONLY | O_TRUNC;
 	perms = S_IRUSR | S_IRGRP | S_IROTH;	// r--r--r--  , writing to input file is not necessary
 	
@@ -25,12 +72,34 @@ int main(int argc, char **argv) {
 	if(numRead == -1) { printf("Error reading input file.\n");  }
 	
 	if(close(filein) == -1) { printf("Error closing input file.\n"); }	// close() error check
-	buf[MAX_BUF] = "\0";
 
+
+	String ch;
+	ch.size = strlen(buf);
 	
+	for (int i=0; i < strlen(buf); i++) {
+		if (buf[i] == NULL) { break; }
+		
+		Char tmp;
+		tmp.c = buf[i];
+		
+		if (!ch.contains(tmp.c)) {
+			for (int j=i; i < strlen(buf); j++) {
+				if (tmp.c == buf[j]) { tmp.count++; }
+			}
+			
+			if (ch.add(tmp) == 0) {
+				printf("Error adding to String struct.");
+			}
+		} else {
+			(ch.find(tmp.c))++;
+		}
+	}
+
+	ch.toString();
 
 
-	printf("First char utf-8: %x\n", buf[0]);
+	// printf("First char utf-8: %x\n", buf[0]);
 
 
 
