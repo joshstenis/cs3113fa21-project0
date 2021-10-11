7b1c9b98a3b0a9de47c1376fffedbbcc5531f2da
joshstenis <joshstenis@gmail.com>
2021-10-11 20:06:59 +0000

    Removed String struct, reorganizing to simpler methodology.

--- project0.c
+++ project0.c
@@ -10,46 +10,6 @@ struct Char {
 };
 typedef struct Char Char;
 
-struct String {
-	int len = 0;
-	int size;
-	Char str[size];
-
-	int contains(Char c) {
-		for (int i=0; i < size; i++) {
-			if (str[i].c == (int)c.c) { return 1; }
-		} return 0;
-	}
-
-	int contains(char c) {
-		for (int i=0; i < size; i++) {
-			if (str[i].c == (int)c) { return 1; }
-		} return 0;
-	}
-
-	Char find(char c) {
-		for (int i=0; i < size; i++) {
-			if (str[i].c == (int)c) { return str[i]; }
-		} return NULL;
-	}
-
-	int add(char c, int count) {
-		Char tmp;
-		tmp.c = c;
-		tmp.count = count;
-		if (len > 0 && len < size) {
-			str[len] = tmp;
-			len++;
-			return 0;
-		} else { return -1; }
-	}
-
-	void toString() {
-		for (int i=0; i < size; i++) { printf("Characters: %c\n", str[i].c); }
-	}
-};
-typedef struct String String;
-
 
 int main(int argc, char **argv) {
 	
@@ -74,29 +34,22 @@ int main(int argc, char **argv) {
 	if(close(filein) == -1) { printf("Error closing input file.\n"); }	// close() error check
 
 
-	String ch;
-	ch.size = strlen(buf);
-	
 	for (int i=0; i < strlen(buf); i++) {
 		if (buf[i] == NULL) { break; }
 		
 		Char tmp;
 		tmp.c = buf[i];
 		
-		if (!ch.contains(tmp.c)) {
+		if () {
 			for (int j=i; i < strlen(buf); j++) {
 				if (tmp.c == buf[j]) { tmp.count++; }
 			}
 			
-			if (ch.add(tmp) == 0) {
+			if () {
 				printf("Error adding to String struct.");
 			}
-		} else {
-			(ch.find(tmp.c))++;
-		}
+		} else {}
 	}
-
-	ch.toString();
 
 
 	// printf("First char utf-8: %x\n", buf[0]);
