ed138bc183c78283ca092511184f9a99fed9121a
joshstenis <joshstenis@gmail.com>
2021-10-13 22:35:35 +0000

    I did itgit add .git add . Shoutout to Mark King, incredible bass grooves are very good for coding. Time to sort.

--- nproj.c
+++ nproj.c
@@ -19,55 +19,60 @@ int main(int argc, char **argv) {
 
 	Char ch[MAX_BYTES];
 
-	int idx = 0;
+	int idx, nbytes = 0;
 	while ((numRead = read(0, buf, 1)) > 0) {
 	
+		buf[1] = '\0';
 		int found = 0;
-		int nbytes = 1;
+		nbytes = 1;
 
 		char hex[8];
-		char cbuf[1];
-		sprintf(hex, "%x", buf[0]);
-		
-		if (hex[6] == 'f') {
-			numRead = read(0, cbuf, 1);
-			buf[1] = cbuf[0];
-			numRead = read(0, cbuf, 1);
-			buf[2] = cbuf[0];
-			numRead = read(0, cbuf, 1);
-			buf[3] = cbuf[0];
-			buf[4] = '\0';
-			nbytes = 4;
-		} else if (hex[6] == 'e') {
-			numRead = read(0, cbuf, 1);
-			buf[1] = cbuf[0];
-			numRead = read(0, cbuf, 1);
-			buf[2] = cbuf[0];
-			buf[3] = '\0';
-			nbytes = 3;
-		} else if (hex[6] == 'd' || hex[6] == 'c') {
-			numRead = read(0, cbuf, 1);
-			buf[1] = cbuf[0];
-			buf[2] = '\0';
-			nbytes = 2;
-		} else { buf[1] = '\0'; }
+		char *cbuf;
+		sprintf(hex, "%08x", buf[0]);
 
-		
-		for (int i=0; i < idx; i++) {
-			if (strncmp(ch[i].c, buf, nbytes) == 0) {
-				ch[i].count++;
-				found = 1;
-				break;
+
+		if (strcmp(hex, "0000000a") != 0 && strcmp(hex, "00000020") != 0) {		
+			if (hex[6] == 'f') {
+				numRead = read(0, &cbuf, 1);
+				buf[1] = cbuf;
+				numRead = read(0, &cbuf, 1);
+				buf[2] = cbuf;
+				numRead = read(0, &cbuf, 1);
+				buf[3] = cbuf;
+				buf[4] = '\0';
+				nbytes = 4;
+			} else if (hex[6] == 'e') {
+				numRead = read(0, &cbuf, 1);
+				buf[1] = cbuf;
+				numRead = read(0, &cbuf, 1);
+				buf[2] = cbuf;
+				buf[3] = '\0';
+				nbytes = 3;
+			} else if (hex[6] == 'd' || hex[6] == 'c') {
+				numRead = read(0, &cbuf, 1);
+				buf[1] = cbuf;
+				buf[2] = '\0';
+				nbytes = 2;
 			}
 
-		} if (found == 0) {
-			strncpy(ch[idx].c, buf, nbytes);
-			ch[idx].c[nbytes] = '\0';
-			ch[idx].count = 1;
-			idx++;
+
+			for (int i=0; i < idx; i++) {
+				if (strncmp(ch[i].c, buf, nbytes) == 0) {
+					ch[i].count++;
+					found = 1;
+					break;
+				}
+	
+			} if (found == 0) {
+				strncpy(ch[idx].c, buf, nbytes);
+				ch[idx].c[nbytes] = '\0';
+				ch[idx].count = 1;
+				idx++;
+			} strcpy(hex, "");
 		}
 	}
 
+	if (numRead < 0) { printf("Read error.\n"); }
 
 
 
