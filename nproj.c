0f4a2bb9bcb1d030dbf7a3aebb77d878667c60fb
joshstenis <joshstenis@gmail.com>
2021-10-13 23:03:37 +0000

    Ironing out qsort.

--- nproj.c
+++ nproj.c
@@ -32,47 +32,46 @@ int main(int argc, char **argv) {
 		char *cbuf;
 		sprintf(hex, "%08x", buf[0]);
 
+	
+		if (hex[6] == 'f') {
+			numRead = read(0, &cbuf, 1);
+			buf[1] = cbuf;
+			numRead = read(0, &cbuf, 1);
+			buf[2] = cbuf;
+			numRead = read(0, &cbuf, 1);
+			buf[3] = cbuf;
+			buf[4] = '\0';
+			nbytes = 4;
+		} else if (hex[6] == 'e') {
+			numRead = read(0, &cbuf, 1);
+			buf[1] = cbuf;
+			numRead = read(0, &cbuf, 1);
+			buf[2] = cbuf;
+			buf[3] = '\0';
+			nbytes = 3;
+		} else if (hex[6] == 'd' || hex[6] == 'c') {
+			numRead = read(0, &cbuf, 1);
+			buf[1] = cbuf;
+			buf[2] = '\0';
+			nbytes = 2;
+		}
 
-		if (strcmp(hex, "0000000a") != 0 && strcmp(hex, "00000020") != 0) {		
-			if (hex[6] == 'f') {
-				numRead = read(0, &cbuf, 1);
-				buf[1] = cbuf;
-				numRead = read(0, &cbuf, 1);
-				buf[2] = cbuf;
-				numRead = read(0, &cbuf, 1);
-				buf[3] = cbuf;
-				buf[4] = '\0';
-				nbytes = 4;
-			} else if (hex[6] == 'e') {
-				numRead = read(0, &cbuf, 1);
-				buf[1] = cbuf;
-				numRead = read(0, &cbuf, 1);
-				buf[2] = cbuf;
-				buf[3] = '\0';
-				nbytes = 3;
-			} else if (hex[6] == 'd' || hex[6] == 'c') {
-				numRead = read(0, &cbuf, 1);
-				buf[1] = cbuf;
-				buf[2] = '\0';
-				nbytes = 2;
+
+		for (int i=0; i < idx; i++) {
+			if (strncmp(ch[i].c, buf, nbytes) == 0) {
+				ch[i].count++;
+				found = 1;
+				break;
 			}
 
-
-			for (int i=0; i < idx; i++) {
-				if (strncmp(ch[i].c, buf, nbytes) == 0) {
-					ch[i].count++;
-					found = 1;
-					break;
-				}
-	
-			} if (found == 0) {
-				strncpy(ch[idx].c, buf, nbytes);
-				ch[idx].c[nbytes] = '\0';
-				ch[idx].count = 1;
-				idx++;
-			} strcpy(hex, "");
-		}
+		} if (found == 0) {
+			strncpy(ch[idx].c, buf, nbytes);
+			ch[idx].c[nbytes] = '\0';
+			ch[idx].count = 1;
+			idx++;
+		} strcpy(hex, "");
 	}
+
 
 	if (numRead < 0) { printf("Read error.\n"); }
 
@@ -83,11 +82,11 @@ int main(int argc, char **argv) {
 	} return 0;
 }
 
-int cmpcount(const void *a, const void *b) {
+int cmpcount(const void *l, const void *r) {
 
-	a = (const Char *)a;
-	b = (const Char *)b;
+	const Char *a = (const Char *)l;
+	const Char *b = (const Char *)r;
 
-	return (*a).count - (*b).count;
+	return a->count - b->count;
 }
 
